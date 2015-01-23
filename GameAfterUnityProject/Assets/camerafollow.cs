1e68595f630d01d65b9e9dc6732a48d68600e743
Toxjr <tox_jr@chello.at>
2015-01-24 00:03:18 +0100

    camera follow script

---
+++ GameAfterUnityProject/Assets/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerafollow : MonoBehaviour {
+
+	public GameObject player;
+	public float offsetX = 0f;
+	public float offsetY = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = new Vector3(player.transform.position.x + offsetX, player.transform.position.y + offsetY,-10);
+		this.transform.position=playerpos;
+
+
+	}
+}
