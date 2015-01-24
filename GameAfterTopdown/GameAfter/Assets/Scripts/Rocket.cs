d4f0c80e3f62966ec7be596a15a4a5804efdfa55
vyanor <vyanor@gmail.com>
2015-01-24 13:41:43 +0100

    prefabs update

--- GameAfterTopdown/GameAfter/Assets/Scripts/Rocket.cs
+++ GameAfterTopdown/GameAfter/Assets/Scripts/Rocket.cs
@@ -4,20 +4,20 @@ using System.Collections;
 public class Rocket : MonoBehaviour 
 {
 	public GameObject explosion;		// Prefab of explosion effect.
-
-
+	
+	
 	void Start () 
 	{
 		// Destroy the rocket after 2 seconds if it doesn't get destroyed before then.
 		Destroy(gameObject, 2);
 	}
-
-
+	
+	
 	void OnExplode()
 	{
 		// Create a quaternion with a random rotation in the z-axis.
 		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
-
+		
 		// Instantiate the explosion where the rocket is with the random rotation.
 		Instantiate(explosion, transform.position, randomRotation);
 	}
@@ -25,14 +25,25 @@ public class Rocket : MonoBehaviour
 	void OnTriggerEnter2D (Collider2D col) 
 	{
 		// If it hits an enemy...
-		if(col.tag == "Enemy")
+		if (col.tag == "Enemy") 
 		{
 			// ... find the Enemy script and call the Hurt function.
-			col.gameObject.GetComponent<Enemy>().Hurt();
-
+			col.gameObject.GetComponent<Enemy> ().Hurt ();
+			
 			// Call the explosion instantiation.
-			OnExplode();
-
+			OnExplode ();
+			
+			// Destroy the rocket.
+			Destroy (gameObject);
+		} 
+		else if (col.tag == "box_destructible") 
+		{
+			// ... find the Enemy script and call the Hurt function.
+			col.gameObject.GetComponent<BoxDestructible> ().Hit ();
+			
+			// Call the explosion instantiation.
+			OnExplode ();
+			
 			// Destroy the rocket.
 			Destroy (gameObject);
 		}
@@ -41,10 +52,10 @@ public class Rocket : MonoBehaviour
 		{
 			// ... find the Bomb script and call the Explode function.
 			col.gameObject.GetComponent<Bomb>().Explode();
-
+			
 			// Destroy the bomb crate.
 			Destroy (col.transform.root.gameObject);
-
+			
 			// Destroy the rocket.
 			Destroy (gameObject);
 		}
